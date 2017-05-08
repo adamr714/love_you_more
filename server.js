20bab4e8f53e39d760f6f20909c55581b6355906
Adam Rosenblum <AdamR714@gmail.comgit config --global user.emailgit config --global user.email AdamR714@gmail.com>
2017-05-07 17:57:07 -0700

    First Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
