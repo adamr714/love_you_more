f6474cf91cc965f2087c43c925f0a5ff2c79a2ad
Adam Rosenblum <AdamR714@gmail.comgit config --global user.emailgit config --global user.email AdamR714@gmail.com>
2017-05-17 21:44:18 -0700

    added login form and heroku functionality

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost:27017/ILoveYouMore';
+                    //   'mongodb://localhost:27017/ILoveYouMore';
+                          'mongodb://localhost/iloveyoumore';
+
 exports.PORT = process.env.PORT || 8080;
